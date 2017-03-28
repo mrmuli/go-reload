4aa5d057cb86edf10586f5afd26b538c2be9d58d
andela-jmuli <joseph.muli@andela.com>
2017-03-28 16:50:42 +0300

    Add simple go server

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World \n")
+	fmt.Fprintf(w, "Serve me up :-) \n")
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", index)
+	log.Fatal(http.ListenAndServe(":4000", router))
+}
